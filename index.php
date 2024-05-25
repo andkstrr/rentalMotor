443880b483787b7a65f9f9a4db6a6be964afcc70
andkstrr <andikasatrionurcahyo@gmail.com>
2024-05-25 14:27:24 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rental Motor</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <h4 class="text-center mt-3">Rental Motor</h4>
+                <img src="assets/headerRental.jpeg" class="card-img-top" alt="Motor Image">
+                <div class="card-body">
+                    <form method="POST">
+                        <div class="form-group">
+                            <label for="nama">Nama Pelanggan</label>
+                            <input type="text" id="nama" name="nama" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="waktu">Lama Waktu Rental (per hari)</label>
+                            <input type="number" id="waktu" name="waktu" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="jenis">Jenis Motor</label>
+                            <select name="jenis" id="jenis" class="form-control" required>
+                                <option disabled selected>-- Pilih Motor --</option>
+                                <option value="Matic">Motor Matic</option>
+                                <option value="Manual">Motor Manual</option>
+                                <option value="Sport">Motor Sport</option>
+                                <option value="Listrik">Motor Listrik</option>
+                            </select>
+                        </div>
+                        <button type="submit" name="btn-submit" class="btn btn-primary btn-block">Submit</button>
+                    </form>
+                    <?php 
+                    require 'logicRent.php';
+                    if (isset($_POST['btn-submit'])) {
+                        $nama = $_POST['nama'];
+                        $lamaRental = $_POST['waktu'];
+                        $jenisMotor = $_POST['jenis'];
+
+                        $rental = new RentalMotor($nama, $lamaRental, $jenisMotor);
+                        $strukRental = $rental->getHarga();
+                        echo "<div class='mt-3 alert alert-info'>$strukRental</div>";
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ logicRent.php
@@ -0,0 +1,52 @@
+<?php
+
+class RentalMotor {
+        protected $nama,
+                $lamaRental,
+                $jenisMotor,
+                $hargaRental = [
+                        'Matic' => 70000,
+                        'Manual' => 60000,
+                        'Sport' => 10000,
+                        'Listrik' => 85000,
+                ],
+                $ppn = 10000,
+                $diskonMember = 0.05,
+                $hargaTotal;
+
+        public function __construct($nama, $lamaRental, $jenisMotor) {
+                $this->nama = $nama;
+                $this->lamaRental = $lamaRental;
+                $this->jenisMotor = $jenisMotor;
+        }
+
+        public function getHarga() {
+                $dataHarga = $this->hargaRental[$this->jenisMotor];
+                $hargaBeli = $dataHarga * $this->lamaRental;
+                $ppn = $this->ppn;
+                $totalHarga = $hargaBeli + $ppn;
+
+                # Cek User adalah Member
+                $namaMember = ["andika", "aca",  "cahyo",  "alaisya"];
+                $isMember = in_array(strtolower($this->nama), $namaMember);
+
+                if ($isMember) {
+                        $diskon = $totalHarga * $this->diskonMember;
+                        $this->hargaTotal = $totalHarga - $diskon;
+                        return "Pelanggan $this->nama berstatus sebagai member, <br> 
+                        Mendapatkan diskon sebesar 5% <br> 
+                        Jenis Motor yang dirental : Motor $this->jenisMotor <br> 
+                        Lama waktu rental : $this->lamaRental hari <br>
+                        Total harga yang harus dibayarkan : " . number_format($this->hargaTotal, 0, '', '.');
+                } else {
+                        $this->hargaTotal = $totalHarga;
+                        return "Pelanggan $this->nama tidak berstatus member. <br>
+                        Jenis Motor yang dirental : Motor $this->jenisMotor <br>
+                        Lama waktu rental $this->lamaRental hari <br>
+                        Total harga yang harus dibayarkan : " . number_format($this->hargaTotal, 0, '', '.');
+                }
+        }
+
+}
+
+?>
